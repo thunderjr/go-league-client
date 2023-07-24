38f16a9fb79a9bdee9b56c8265e2b547e7488e5b
Flavio Marques <flavinhojr2000@gmail.com>
2023-07-24 00:33:59 -0300

    chore: test GET current summoner

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/thunderjr/go-league-client/auth"
+	league_http "github.com/thunderjr/go-league-client/http"
 )
 
 func main() {
@@ -20,5 +22,21 @@ func main() {
 
 	leagueAuth.Authenticate(ctx)
 
-	fmt.Println(leagueAuth.Credentials)
+	httpClient := league_http.NewLeagueClient(leagueAuth.Credentials)
+
+	endpoint := "/lol-summoner/v1/current-summoner"
+
+	body, err := httpClient.Get(endpoint)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(string(body))
+
+	body, err = httpClient.Get(endpoint)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(string(body))
 }
