08a6b3b4e578edb0d1a19fc314d996530840b2b9
Flavio Marques <flavinhojr2000@gmail.com>
2023-07-26 23:20:20 -0300

    refactor: move parse ws message to its own file

---
+++ websocket/parse-message.go
@@ -0,0 +1,36 @@
+package league_websocket
+
+import (
+	"encoding/json"
+)
+
+func parseMessage(message []byte) (EventResponse, error) {
+	/*
+	 * The websocket message response is a slice with the following (in order):
+	 * 	- EventType int
+	 * 	- EventName string (here it's always == 'OnJsonApiEvent')
+	 * 	- Data map[string]interface{} (actual useful data)
+	 *			= The keys for the Data map are:
+	 *				- data
+	 *				- eventType
+	 *				- uri
+	 */
+	res := []interface{}{
+		0,                            // EventType
+		"",                           // EventName
+		make(map[string]interface{}), // Data
+	}
+
+	if err := json.Unmarshal(message, &res); err != nil {
+		return EventResponse{}, err
+	}
+
+	resData := res[2].(map[string]interface{})
+
+	eventResponse := EventResponse{
+		Data: resData["data"],
+		URI:  resData["uri"].(string),
+	}
+
+	return eventResponse, nil
+}
