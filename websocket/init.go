cf699a1f3c8f26acda1aae24a23caa1adfa6f7ce
Flavio Marques <flavinhojr2000@gmail.com>
2023-07-26 23:22:46 -0300

    refactor: websocket client init

--- websocket/init.go
+++ websocket/init.go
@@ -11,8 +11,6 @@ import (
 	"log"
 	"net/http"
 	"net/url"
-	"reflect"
-	"strings"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -50,82 +48,17 @@ func createWebSocketConnection(address string, authHeader http.Header, dialer *w
 	return conn, nil
 }
 
-func (lws *LeagueWebSocket) listen() {
-	for {
-		_, message, err := lws.Conn.ReadMessage()
-		if err != nil {
-			log.Println("[listen] read ws message:", err)
-			return
-		}
-
-		/*
-		 * The websocket message response is a slice with the following (in order):
-		 * 	- EventType int
-		 * 	- EventName string (here it's always == 'OnJsonApiEvent')
-		 * 	- Data map[string]interface{} (actual useful data)
-		 *		= The keys for the Data map are:
-		 *			- data
-		 *			- eventType
-		 *			- uri
-		 */
-
-		// res := make([]interface{}, 0, 3)
-		res := []interface{}{
-			0,                            // EventType
-			"",                           // EventName
-			make(map[string]interface{}), // Data
-		}
-
-		// The subscription to the OnJsonApiEvent returns an empty slice as response
-		// taking care of it here
-		if reflect.ValueOf(message).Kind() == reflect.Slice {
-			slice := reflect.ValueOf(message)
-			if slice.Len() == 0 {
-				continue
-			}
-		}
-
-		if err := json.Unmarshal(message, &res); err != nil {
-			log.Println("[listen] unmarshal event:", err)
-			continue
-		}
-
-		// Accessing the event data structure
-		resData := res[2].(map[string]interface{})
-
-		eventResponse := EventResponse{
-			Data: resData["data"],
-			URI:  resData["uri"].(string),
-		}
-
-		if callbacks, ok := lws.Subscriptions[eventResponse.URI]; ok {
-			for _, cb := range callbacks {
-				cb(eventResponse)
-			}
-		}
-	}
-}
-
-func (lws *LeagueWebSocket) Subscribe(path string, effect EventCallback) {
-	p := strings.Trim(path, " ")
-	if !strings.HasPrefix(p, "/") {
-		p = "/" + p
+func (lws *LeagueWebSocket) subscribeToJsonEvents() error {
+	subscriptionMessage, err := json.Marshal([]interface{}{5, "OnJsonApiEvent"})
+	if err != nil {
+		return err
 	}
 
-	if _, ok := lws.Subscriptions[p]; !ok {
-		lws.Subscriptions[p] = []EventCallback{effect}
-	} else {
-		lws.Subscriptions[p] = append(lws.Subscriptions[p], effect)
-	}
-}
-
-func (lws *LeagueWebSocket) Unsubscribe(path string) {
-	p := strings.Trim(path, " ")
-	if !strings.HasPrefix(p, "/") {
-		p = "/" + p
+	if err := lws.Conn.WriteMessage(websocket.TextMessage, subscriptionMessage); err != nil {
+		return err
 	}
 
-	delete(lws.Subscriptions, p)
+	return nil
 }
 
 func Init(options *ConnectionOptions) (*LeagueWebSocket, error) {
@@ -175,13 +108,8 @@ func Init(options *ConnectionOptions) (*LeagueWebSocket, error) {
 		Subscriptions: make(map[string][]EventCallback),
 	}
 
-	subscriptionMessage, err := json.Marshal([]interface{}{5, "OnJsonApiEvent"})
-	if err != nil {
-		return nil, err
-	}
-
-	if err := lws.Conn.WriteMessage(websocket.TextMessage, subscriptionMessage); err != nil {
-		return nil, err
+	if err := lws.subscribeToJsonEvents(); err != nil {
+		return nil, errors.New("could not subscribe to json events")
 	}
 
 	go lws.listen()
